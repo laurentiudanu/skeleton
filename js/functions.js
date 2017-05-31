626c318979a036f45ea640d239e95e7d17b211f7
Laurentiu Danu <laurentiu.danu@gmail.com>
2017-05-31 09:31:33 +0300

    initial commit

---
+++ js/functions.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+  (function($){
+    
+
+    
+  })(jQuery);
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  function is_touch_device() {
+    return (('ontouchstart' in window)
+        || (navigator.MaxTouchPoints > 0)
+        || (navigator.msMaxTouchPoints > 0));
+    }
+  var pHtml = document.getElementsByTagName( 'html' )[0];
+  if (!is_touch_device()) {
+    pHtml.setAttribute( 'class', 'no-touch' );
+  }
+  else {
+    pHtml.setAttribute( 'class', 'touch' );
+  }
+
+});
