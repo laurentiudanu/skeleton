50304f243a2abcb5e0d32c192970d9350b1da3a0
Laurentiu Danu <laurentiu.danu@gmail.com>
2017-06-20 12:24:17 +0300

    SPA test

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,70 @@
 window.onload = function() {
   (function($){
+    var site = $("#app"),
+        db = {
+                "posts": [
+                  { "id": 1, "title": "jsondb is awesome"},
+                  { "id": 2, "title": "some title goes here", "content": "<p>some paragraph</p><h2>an h2</h2>"}
+                ],
+                "meta": [
+                  {"sitename": "test js app", "description": "some fucking amazing description goes here"}
+                ]
+              };
+    for ( var i = 0; i < db.meta.length; i++ ) {
+      var sitename = db.meta[i].sitename,
+          siteDescription = db.meta[i].description;
+      $('title').text(sitename);
+    }
     
-
+    site.append("<p class='site-nav'></p>");
+    site.append("<div class='app-content'></div>");
+    
+    
+    for (i = 1; i <= 10; i++) {
+      $("<div class='indicator-inner'></div>").appendTo(".indicator");
+    }
+    
+    function addNode(type, classname, content) {
+      var nodeType = type,
+          nodeClass = classname,
+          nodeContent = content;
+      
+      if (nodeType == "div") {
+        $("#app").append("<"+nodeType+" class='"+nodeClass+"'>"+nodeContent+"</"+nodeType+">");
+      }
+      else if (nodeType == "p") {
+        $("#app").append("<"+nodeType+" class='"+nodeClass+"'>"+nodeContent+"</"+nodeType+">");
+      }
+      else if (nodeType == "h1") {
+        $("#app").append("<"+nodeType+" class='"+nodeClass+"'>"+nodeContent+"</"+nodeType+">");
+      }
+    }
+    
+    addNode("p", "penis", "cucu");
+    
+    for ( var i = 0; i < db.posts.length; i++ ) {
+      var entryId = db.posts[i].id,
+          entryTitle = db.posts[i].title;
+      $("<span id='id"+entryId+"'>"+entryTitle+"</span>").appendTo(".site-nav");
+    }
+    
+    $(document).on("click", ".site-nav span", function(){
+      var idToF = $(this).attr("id").replace('id','');
+      $(".app-content", site).empty();
+      for ( var i = 0; i < db.posts.length; i++ ) {
+        var entryId = db.posts[i].id,
+            entryTitle = db.posts[i].title,
+            entryContent = db.posts[i].content;
+        if (entryId == idToF) {
+          $(".app-content", site).append("<p>Id 2 found - "+entryTitle+"</p>");
+          $(".app-content", site).append(entryContent);
+          //window.location.hash = '#pid'+entryId;
+        }
+      }
+    });
+    
+    
+    
     
   })(jQuery);
 }
